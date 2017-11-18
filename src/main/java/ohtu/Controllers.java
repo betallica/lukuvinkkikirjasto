efc88888d0d5fe18f8ee5ffda8456108e8042342
Oliver Leppänen <oliverleppanen@gmail.com>
2017-11-18 15:18:34 +0200

    Added BookHintService class and set the controller to return home.html from root path

--- src/main/java/ohtu/Controllers.java
+++ src/main/java/ohtu/Controllers.java
@@ -1,20 +1,27 @@
 package ohtu;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ohtu.database.dto.BookHintDto;
+import ohtu.database.repository.BookHintRepository;
+import ohtu.service.BookHintService;
 
 @Controller
 public class Controllers {
+	
+	@Autowired
+	private BookHintService bhService;
+	
+	@Autowired
+	private BookHintRepository bhRep;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {    	
         return "home";
-    }
-    
-    @GetMapping("/other")
-    @ResponseBody
-    public String other() {
-        return "Hei Maailma!";
     }
 }
