97ce67c9e266b9ea85365eae8e6a19ec4abca72b
Werneri Männynsalo <werneri.mannynsalo@helsinki.fi>
2017-11-21 21:16:49 +0200

    Cucumber tests for adding a book

--- src/test/java/ohtu/Stepdefs.java
+++ src/test/java/ohtu/Stepdefs.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -14,6 +15,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Stepdefs {
 
     WebDriver driver;
+    
+    private final String BASE_URL = "http://localhost:8080";
 
     public Stepdefs() {
         File file;
@@ -32,72 +35,57 @@ public class Stepdefs {
     public void tearDown() {
         driver.quit();
     }
-
-    @Given("^user is at the main page$")
-    public void user_is_at_the_main_page() throws Throwable {
-        driver.get("http://localhost:" + 8080 + "/" );
-        Thread.sleep(1000);        
-    }
-
-    @When("^a link is clicked$")
-    public void a_link_is_clicked() throws Throwable {
-        Thread.sleep(1000);  
-        clickLinkWithText("linkki" );
-        Thread.sleep(1000);  
-    }    
-   
-    @Then("^\"([^\"]*)\" is shown$")
-    public void is_shown(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.tagName("body"))
-                .getText().contains(arg1));
-    }
     
     @Given("^command add book is selected$")
     public void command_add_book_is_selected() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        driver.get(BASE_URL);
+        clickLinkWithText("Lisää kirja");
     }
 
     @When("^valid name \"([^\"]*)\" and valid author \"([^\"]*)\" and valid isbn \"([^\"]*)\" are entered$")
-    public void valid_name_and_valid_author_and_valid_isbn_are_entered(String arg1, String arg2, String arg3) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void valid_name_and_valid_author_and_valid_isbn_are_entered(String name, String author, String isbn) throws Throwable {
+        addBookWith(name, author, isbn);
     }
 
     @Then("^user is redirected to front page$")
     public void user_is_redirected_to_front_page() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        assertTrue(driver.getPageSource().contains("Kirjat"));
     }
-
-    @Then("^the new book is listed$")
-    public void the_new_book_is_listed() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    
+    @Then("^a new book is listed with the isbn \"([^\"]*)\"$")
+    public void a_new_book_is_listed_with_the_isbn(String isbn) throws Throwable {
+        assertTrue(driver.getPageSource().contains(isbn));
     }
 
     @When("^empty name \"([^\"]*)\" and valid author \"([^\"]*)\" and valid isbn \"([^\"]*)\" are entered$")
-    public void empty_name_and_valid_author_and_valid_isbn_are_entered(String arg1, String arg2, String arg3) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void empty_name_and_valid_author_and_valid_isbn_are_entered(String name, String author, String isbn) throws Throwable {
+        addBookWith(name, author, isbn);
     }
 
     @Then("^error message \"([^\"]*)\" is shown$")
-    public void error_message_is_shown(String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void error_message_is_shown(String error_message) throws Throwable {
+        assertTrue(driver.getPageSource().contains(error_message));
     }
 
     @When("^valid name \"([^\"]*)\" and empty author \"([^\"]*)\" and valid isbn \"([^\"]*)\" are entered$")
-    public void valid_name_and_empty_author_and_valid_isbn_are_entered(String arg1, String arg2, String arg3) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void valid_name_and_empty_author_and_valid_isbn_are_entered(String name, String author, String isbn) throws Throwable {
+        addBookWith(name, author, isbn);
     }
 
     @When("^valid name \"([^\"]*)\" and empty author \"([^\"]*)\" and invalid isbn \"([^\"]*)\" are entered$")
-    public void valid_name_and_empty_author_and_invalid_isbn_are_entered(String arg1, String arg2, String arg3) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void valid_name_and_empty_author_and_invalid_isbn_are_entered(String name, String author, String isbn) throws Throwable {
+        addBookWith(name, author, isbn);
+    }
+    
+    private void addBookWith(String name, String author, String isbn) {
+        WebElement element = driver.findElement(By.name("name"));
+        element.sendKeys(name);
+        element = driver.findElement(By.name("author"));
+        element.sendKeys(author);
+        element = driver.findElement(By.name("isbn"));
+        element.sendKeys(isbn);
+        element = driver.findElement(By.name("submit"));
+        element.click();
     }
 
     private void clickLinkWithText(String text) {
@@ -108,7 +96,7 @@ public class Stepdefs {
                 element.click();
                 break;           
             } catch(Exception e) {
-                System.out.println(e.getStackTrace());
+                e.getStackTrace();
             }
         }
     }
--- src/test/resources/ohtu/adding_books.feature
+++ src/test/resources/ohtu/adding_books.feature
@@ -1,22 +1,22 @@
 Feature: user can add books
-	
-	Scenario: user can add a book
-		Given command add book is selected
-		When valid name "Clean Code" and valid author "Robert C. Martin" and valid isbn "978-0132350884" are entered
-		Then user is redirected to front page
-		And  the new book is listed
-		
-	Scenario: adding book fails with empty name
-		Given command add book is selected
-		When  empty name "" and valid author "Robert C. Martin" and valid isbn "978-0132350884" are entered
-		Then  error message "Name can not be empty" is shown
-		
-	Scenario: adding book fails with empty author
-		Given command add book is selected
-		When  valid name "Clean Code" and empty author "" and valid isbn "978-0132350884" are entered
-		Then  error message "Author can not be empty" is shown
-		
-	Scenario: adding book fails with invalid isbn
-		Given command add book is selected
-		When  valid name "Clean Code" and empty author "Robert C. Martin" and invalid isbn "12345" are entered
-		Then  error message "Invalid isbn" is shown
+
+        Scenario: user can add a book
+                Given command add book is selected
+                When valid name "Clean Code" and valid author "Robert C. Martin" and valid isbn "978-0132350884" are entered
+                Then user is redirected to front page
+                And  a new book is listed with the isbn "978-0132350884"
+
+        Scenario: adding book fails with empty name
+                Given command add book is selected
+                When  empty name "" and valid author "Robert C. Martin" and valid isbn "978-0132350884" are entered
+                Then  error message "Name can not be empty" is shown
+
+        Scenario: adding book fails with empty author
+                Given command add book is selected
+                When  valid name "Clean Code" and empty author "" and valid isbn "978-0132350884" are entered
+                Then  error message "Author can not be empty" is shown
+
+        Scenario: adding book fails with invalid isbn
+                Given command add book is selected
+                When  valid name "Clean Code" and empty author "Robert C. Martin" and invalid isbn "12345" are entered
+                Then  error message "Not a valid isbn" is shown
