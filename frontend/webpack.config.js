1509cb803d5422472036f943d1af67281c7004a7
V-Kopio <ville.kopio@gmail.com>
2017-11-22 15:33:42 +0200

    Fixed webpack.config code style

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
-const static_dir = '../src/main/resources/static'
+const static_dir = "../src/main/resources/static";
 
 module.exports = {
-    entry: './src/js/app.js',
+    entry: "./src/js/app.js",
 
     output: {
         path: path.join(__dirname, static_dir),
-        filename: 'js/app.js',
+        filename: "js/app.js",
     },
 
     module: {
         loaders: [
             {
                 test: /\.scss$/,
-                loader: ExtractTextPlugin.extract('css-loader?minimize=true!sass-loader')
+                loader: ExtractTextPlugin.extract("css-loader?minimize=true!sass-loader")
             }
         ]
     },
@@ -24,7 +24,7 @@ module.exports = {
     plugins: [
         new UglifyJSPlugin(),
         new ExtractTextPlugin(
-            'css/app.css',
+            "css/app.css",
             {
                 allChunks: true
             }
