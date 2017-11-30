ca9f681940fdae9b0d7f279eef569b133a6f20b7
Samuel0c <sleppiniemi@protonmail.com>
2017-11-30 23:58:57 +0200

    small style fix

--- src/main/java/ohtu/database/repository/HintRepository.java
+++ src/main/java/ohtu/database/repository/HintRepository.java
@@ -1,10 +1,6 @@
 package ohtu.database.repository;
 
-import ohtu.model.BookHint; 
 import ohtu.model.Hint;
-
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
