1454eed90c4e423aa971ca006ade32bc7256a68f
V-Kopio <ville.kopio@gmail.com>
2017-12-04 02:43:35 +0200

    Fixed font-awesome file paths

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -20,11 +20,11 @@ module.exports = {
             },
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]"
+                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=/fonts/[name].[ext]"
             },
             { 
                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader?name=fonts/[name].[ext]"
+                loader: "file-loader?name=/fonts/[name].[ext]"
             }
         ]
     },
