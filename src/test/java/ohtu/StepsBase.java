93e93281d691f719736127f3f1199410bd185c30
ikuisma <ilkka.a.kuisma@gmail.com>
2017-12-07 06:53:44 +0200

    Added bean for injecting shared webdriver across step definition files.

---
+++ src/test/java/ohtu/StepsBase.java
@@ -0,0 +1,53 @@
+package ohtu;
+
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StepsBase {
+
+    private class SilentHtmlUnitDriver extends HtmlUnitDriver {
+        SilentHtmlUnitDriver() {
+            super();
+            this.getWebClient().setCssErrorHandler(new SilentCssErrorHandler());
+        }
+    }
+
+    private WebDriver driver;
+    public final String BASE_URL = "http://localhost:8080";
+
+    public StepsBase() {
+        driver = new SilentHtmlUnitDriver();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void goToBaseUrl() {
+        getDriver().get(BASE_URL);
+    }
+
+    public void clickLinkWithText(String text) {
+        int trials = 0;
+        while( trials++<5 ) {
+            try{
+                final WebElement element = getDriver().findElement(By.linkText(text));
+                element.click();
+                break;
+            } catch(Exception e) {
+                e.getStackTrace();
+            }
+        }
+    }
+}
+
+
