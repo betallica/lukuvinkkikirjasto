c3d7f048e18adda9331b713f17d6ce0d905d6a0c
ikuisma <ilkka.a.kuisma@gmail.com>
2017-12-07 06:54:53 +0200

    Removed unused imports.

--- src/test/java/ohtu/RunCukesTest.java
+++ src/test/java/ohtu/RunCukesTest.java
@@ -4,8 +4,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty"})
